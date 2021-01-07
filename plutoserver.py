4491a37d08b02de52cb421d196985f9c24dc34c7
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@intelligencia.ai>
2021-01-07 19:15:29 +0200

    Update plutoserver

--- plutoserver.py
+++ plutoserver.py
@@ -1,3 +1,4 @@
+# Updated 20210107_19:15
 def setup_plutoserver():
   return {
     "command": ["julia", "--project=\".\"",  "--optimize=0",  "-e", "import Pluto; Pluto.run(host=\"0.0.0.0\", port={port}, launch_browser=false, require_secret_for_open_links=false, require_secret_for_access=false, project=pwd())"],
