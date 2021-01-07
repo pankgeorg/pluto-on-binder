b634258d6c81b11c63d950e731477f0ad580e7db
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@intelligencia.ai>
2021-01-07 02:02:24 +0200

    update binder image

--- plutoserver.py
+++ plutoserver.py
@@ -1,7 +1,7 @@
 def setup_plutoserver():
   return {
-    "command": ["julia", "-Jnotebook_sysimage.so", "-e", "import Pluto; Pluto.run(host=\"0.0.0.0\", port={port}, launch_browser=false, require_secret_for_open_links=false, require_secret_for_access=false)"],
-    "timeout": 60,
+    "command": ["julia", "--optimize=0", "-Jnotebook_sysimage.so", "-e", "import Pluto; Pluto.run(host=\"0.0.0.0\", port={port}, launch_browser=false, require_secret_for_open_links=false, require_secret_for_access=false)"],
+    "timeout": 128,
     "launcher_entry": {
         "title": "Pluto.jl",
     },
