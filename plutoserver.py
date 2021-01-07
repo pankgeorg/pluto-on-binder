ba6929d2bf78c86e7139c648e8a522057b604dee
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@intelligencia.ai>
2021-01-07 20:51:46 +0200

    Keep only ref to Dockerfile

--- plutoserver.py
+++
@@ -1,8 +0,0 @@
-def setup_plutoserver():
-  return {
-    "command": ["julia", "--project=\".\"", "--optimize=0",  "-e", "import Pluto; Pluto.run(host=\"0.0.0.0\", port={port}, launch_browser=false, require_secret_for_open_links=false, require_secret_for_access=false, project=pwd())"],
-    "timeout": 32,
-    "launcher_entry": {
-        "title": "Pluto.jl",
-    },
-  }
